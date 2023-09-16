cda32cd65a63b7171040287dca964aa85ec7e0ca
FR0ZENF1RE <137827333+FR0ZENF1RE@users.noreply.github.com>
2023-09-15 17:27:31 -0800

    added patch method to all user info

--- back-end/seeds/01_user.js
+++ back-end/seeds/01_user.js
@@ -3,32 +3,38 @@
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-  // Deletes ALL existing entries
-  await knex("user_table").del();
-  await knex("user_table").insert([
-    {
-      first_name: "John",
-      last_name: "Doe",
-      rank: "Private",
-      role: "Tech",
-      flight: "Bravo",
-      section: "First",
-    },
-    {
-      first_name: "2John",
-      last_name: "2Doe",
-      rank: "Private",
-      role: "Tech",
-      flight: "Bravo",
-      section: "First",
-    },
-    {
-      first_name: "3John",
-      last_name: "3Doe",
-      rank: "Private",
-      role: "Tech",
-      flight: "Bravo",
-      section: "First",
-    },
-  ]);
+	// Deletes ALL existing entries
+	await knex('user_table').del();
+	await knex('user_table').insert([
+		{
+			username: 'admin',
+			password: 'admin',
+			first_name: 'John',
+			last_name: 'Doe',
+			rank: 'Private',
+			role: 'Tech',
+			flight: 'Bravo',
+			section: 'First',
+		},
+		{
+			username: 'jd200',
+			password: 'password',
+			first_name: '2John',
+			last_name: '2Doe',
+			rank: 'Private',
+			role: 'Tech',
+			flight: 'Bravo',
+			section: 'First',
+		},
+		{
+			username: 'jd300',
+			password: 'mypassword',
+			first_name: '3John',
+			last_name: '3Doe',
+			rank: 'Private',
+			role: 'Tech',
+			flight: 'Bravo',
+			section: 'First',
+		},
+	]);
 };
