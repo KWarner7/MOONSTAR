38aa6c45e95316ff55465c9cdb3a660448e83366
FR0ZENF1RE <137827333+FR0ZENF1RE@users.noreply.github.com>
2023-09-19 11:46:32 -0800

    route added for edit project

--- front-end/moonstar/src/App.js
+++ front-end/moonstar/src/App.js
@@ -1,28 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Routes, Navigate, useLocation } from "react-router-dom";
+import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import HomePage from './HomePage.js';
 import Login from './Login.js';
 import ActiveProjects from './ActiveProjects.js';
 import CompletedProjects from './CompletedProjects.js';
+import EditProject from './EditProject';
 
 function App() {
-  return (
-    <Router>
-    <div className="App">
-      <header className="App-header">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/active-projects" element={<ActiveProjects />} />
-          <Route path="/completed-projects" element={<CompletedProjects />} />
-        </Routes>
-      </header>
-    </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<header className='App-header'>
+					<Routes>
+						<Route path='/' element={<HomePage />} />
+						<Route path='/login' element={<Login />} />
+						<Route path='/active-projects' element={<ActiveProjects />} />
+						<Route path='/completed-projects' element={<CompletedProjects />} />
+						<Route path='/edit-project' element={<EditProject />} />
+					</Routes>
+				</header>
+			</div>
+		</Router>
+	);
 }
-
 
 export default App;
